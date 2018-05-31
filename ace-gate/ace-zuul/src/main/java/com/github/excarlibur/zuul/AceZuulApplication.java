9f6a41923359b8966583d2f2824a6c35b1d80065
chenxuewei <chenxuewei@hzquanweitech.com>
2018-05-31 11:25:42 +0800

    feat(ace-zuul): 网关服务

---
+++ ace-gate/ace-zuul/src/main/java/com/github/excarlibur/zuul/AceZuulApplication.java
@@ -0,0 +1,12 @@
+package com.github.excarlibur.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceZuulApplication.class, args);
+	}
+}
