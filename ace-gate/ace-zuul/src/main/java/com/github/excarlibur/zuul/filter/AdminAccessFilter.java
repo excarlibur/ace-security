00025252520c8f88d52cb96bb54ba72b71f6625f
chenxuewei <chenxuewei@hzquanweitech.com>
2018-06-04 17:33:38 +0800

    feat(ace-zuul): 网关服务

---
+++ ace-gate/ace-zuul/src/main/java/com/github/excarlibur/zuul/filter/AdminAccessFilter.java
@@ -0,0 +1,50 @@
+package com.github.excarlibur.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by chenxuewei on 2018/6/4.
+ */
+@Component
+public class AdminAccessFilter extends ZuulFilter {
+
+  private static Logger log = LoggerFactory.getLogger(AdminAccessFilter.class);
+
+  @Override
+  public String filterType() {
+    return "pre";
+  }
+
+  @Override
+  public int filterOrder() {
+    return 0;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+
+  @Override
+  public Object run() {
+    RequestContext ctx = RequestContext.getCurrentContext();
+    HttpServletRequest request = ctx.getRequest();
+
+    log.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+
+    Object accessToken = request.getParameter("accessToken");
+    if(accessToken == null) {
+      log.warn("access token is empty");
+      ctx.setSendZuulResponse(false);
+      ctx.setResponseStatusCode(401);
+      return null;
+    }
+    log.info("access token ok");
+    return null;
+  }
+}
